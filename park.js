1d0c292514109822b3fcad023d8fa6f8377f7810
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-03 20:00:01 +0100

    Add addDinosaur function to park.js 'should be able to add dinosaur' passing test

--- park.js
+++ park.js
@@ -2,4 +2,8 @@ var Park = function(){
   this.enclosure = [];
 }
 
+Park.prototype.addDinosaur = function (dinosaur) {
+  this.enclosure.push(dinosaur);
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,19 +1,27 @@
 var assert = require('assert');
 var Park = require('../park.js');
+var Dinosaur = require('../dinosaur.js');
 
 describe('Park', function(){
 
   var park1;
+  var dino1;
+  var dino2;
 
   beforeEach(function(){
     park1 = new Park();
+    dino1 = new Dinosaur('Tyrannosaurus', 2);
+    dino2 = new Dinosaur('Velociraptor', 4);
   });
 
   it('enclosure should start empty', function(){
     assert.strictEqual(park1.enclosure.length, 0);
   });
 
-  //should be able to add dinosaur
+  it('should be able to add dinosaur', function(){
+    park1.addDinosaur(dino1);
+    assert.strictEqual(park1.enclosure.length, 1);
+  });
 
   //should be able to remove all dinosaurs of a particular type
 
