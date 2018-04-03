cfc891deb6bfcf0eebfedf8b070cc0a70ea1f6f4
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-03 19:25:12 +0100

    Add properties to Dinosaur and create a beforeEach in dinosaur_spec.js

--- dinosaur.js
+++ dinosaur.js
@@ -1,3 +1,6 @@
-
+var Dinosaur = function(type, yearlyOffspring){
+  this.type = type;
+  this.yearlyOffspring = yearlyOffspring;
+}
 
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -3,4 +3,16 @@ var Dinosaur = require('../dinosaur.js');
 
 describe('Dinosaur', function(){
 
+  var dino1;
+  var dino2;
+
+  beforeEach(function(){
+    dino1 = new Dinosaur('Tyrannosaurus', 2);
+    dino2 = new Dinosaur('Velociraptor', 4);
+  });
+
+  it('a dinosaur should have a type', function(){
+
+  });
+
 });
