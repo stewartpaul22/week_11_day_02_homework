c1fe3e8062f58c0a9a1c577a2918ac433f70dbaa
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-04 08:31:29 +0100

    Add reproduce() to park, but not working

--- park.js
+++ park.js
@@ -2,8 +2,10 @@ var Park = function(){
   this.enclosure = [];
 }
 
-Park.prototype.addDinosaur = function (dinosaur) {
-  this.enclosure.push(dinosaur);
+Park.prototype.addDinosaur = function (dinosaur, quantity) {
+  for (var i = 0; i < quantity; i++) {
+    this.enclosure.push(dinosaur);
+  }
 };
 
 Park.prototype.removeDinosaursByType = function (dinoType) {
@@ -13,6 +15,16 @@ Park.prototype.removeDinosaursByType = function (dinoType) {
 Park.prototype.listDinosWithOffspringGreaterThan = function (offspringCount) {
   let dinoList = this.enclosure.filter(dino => dino.yearlyOffspring > offspringCount);
   return dinoList;
+};
+
+Park.prototype.reproduce = function (numberOfYears) {
+  let total = 0;
+  for (let i = 0; i < numberOfYears; i++) {
+    for (let dino of this.enclosure) {
+      total += (dino.yearlyOffspring + 1);
+    }
+  }
+
 };
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -20,24 +20,30 @@ describe('Park', function(){
   });
 
   it('should be able to add dinosaur', function(){
-    park1.addDinosaur(dino1);
+    park1.addDinosaur(dino1, 1);
     assert.strictEqual(park1.enclosure.length, 1);
   });
 
   it('can remove all dinosaurs of a particular type', function(){
-    park1.addDinosaur(dino1);
-    park1.addDinosaur(dino2);
-    park1.addDinosaur(dino3);
+    park1.addDinosaur(dino1, 1);
+    park1.addDinosaur(dino2, 2);
+    // park1.addDinosaur(dino3);
     park1.removeDinosaursByType('Velociraptor');
     assert.strictEqual(park1.enclosure.length, 1);
   });
 
   it('get all dinosaurs with offspring count more than 2', function(){
-    park1.addDinosaur(dino1);
-    park1.addDinosaur(dino2);
-    park1.addDinosaur(dino3);
+    park1.addDinosaur(dino1, 1);
+    park1.addDinosaur(dino2, 2);
+    // park1.addDinosaur(dino3);
     let dinoList = park1.listDinosWithOffspringGreaterThan(2);
     assert.strictEqual(dinoList.length, 2);
+  });
+
+  it('get number of dinosaurs after year 1 with 1 dinosaur', function(){
+    park1.addDinosaur(dino1, 1);
+    park1.reproduce(1);
+    assert.strictEqual(park1.enclosure.length, 3);
   });
 
 });
